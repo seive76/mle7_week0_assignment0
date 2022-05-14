29c334f7f134b33871b65eb9b068fa66da593fc4
jongbumlee <seive76@gmail.com>
2022-05-14 10:41:52 +0900

    Created for 2nd practice made on 14th May.

---
+++ src2/add.py
@@ -0,0 +1,11 @@
+def add(x:int, y:int=100) -> int:
+    """
+    Add two numbers together.
+
+    Args:
+        x (int): The first number.
+        y (int): The second number. Defaults to 100.
+    Returns:
+        int: The sum of the values of squared two numbers.
+    """
+    return x*x + y*y
